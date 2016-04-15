19c7ed2b7ea43400d69dab1aaeec71c09ffa2b81
shanhong cheng <shanhong.cheng@utah.edu>
2016-04-15 13:38:30 -0600

    title

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if(file_exists('config.php')){
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Local</title>
+        <title>Bootstrap Template</title>
         <meta name="viewport" content="width=device-width">
         <link rel="stylesheet" href="css/main.css">
     </head>
